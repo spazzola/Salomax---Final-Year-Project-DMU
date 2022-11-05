f9afd0f577e97b5d4401f324f609d5373b895a4e
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-05 15:57:01 +0000

    Added Address validation

--- src/main/java/Salomax/address/AddressService.java
+++ src/main/java/Salomax/address/AddressService.java
@@ -1,12 +1,38 @@
 package Salomax.address;
 
+import Salomax.userDetails.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AddressService {
 
-    public boolean validateAddress() {
-        return true;
+    private UserService userService;
+
+
+    public String validateAddress(AddressDto addressDto) {
+        String messageException = "";
+        if (!userService.validateName(addressDto.getCountry())) {
+            messageException = "Bad value of country. ";
+        }
+        if (!userService.validateName(addressDto.getVoivodeship())) {
+            messageException = "Bad value of voivodeship. ";
+        }
+        if (!userService.validateName(addressDto.getCity())) {
+            messageException += "Bad value of city. ";
+        }
+        if (!userService.validateName(addressDto.getPostalCode())) {
+            messageException += "Bad value of postal code. ";
+        }
+        if (!userService.validateName(addressDto.getStreet())) {
+            messageException += "Bad value of street ";
+        }
+        if (!userService.validateName(addressDto.getHouseNumber())) {
+            messageException += "Bad value of house number. ";
+        }
+
+        return messageException;
     }
 
 }
