82f09d066fb2b78367e8e89e610de06acbc49933
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-05 15:57:31 +0000

    Added AddressDto to CreateStudioRequest

--- src/main/java/Salomax/studio/CreateStudioRequest.java
+++ src/main/java/Salomax/studio/CreateStudioRequest.java
@@ -1,6 +1,8 @@
 package Salomax.studio;
 
 import Salomax.address.Address;
+import Salomax.address.AddressDto;
+import Salomax.employee.EmployeeDto;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,18 +10,8 @@ import lombok.Getter;
 @Builder
 public class CreateStudioRequest {
 
-    private String studioName;
-    private String nip;
-    private String regon;
-    private String studioPhoneNumber;
-    private String studioEmail;
-    private Address address;
-
-    private String login;
-    private String password;
-    private String employeeName;
-    private String surname;
-    private String employeePhoneNumber;
-    private String employeeEmail;
+    private StudioDto studioDto;
+    private EmployeeDto employeeDto;
+    private AddressDto addressDto;
 
 }
