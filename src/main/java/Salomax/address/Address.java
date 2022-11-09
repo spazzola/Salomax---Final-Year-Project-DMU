f0110f209b5078b52acb07e8b16b478823f84401
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-09 18:00:59 +0000

    Added method to comparing Address objects

--- src/main/java/Salomax/address/Address.java
+++ src/main/java/Salomax/address/Address.java
@@ -2,6 +2,8 @@ package Salomax.address;
 
 import lombok.Builder;
 
+import java.util.Objects;
+
 @Builder
 public class Address {
 
@@ -12,4 +14,21 @@ public class Address {
     private String street;
     private String houseNumber;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(country, address.country) &&
+                Objects.equals(voivodeship, address.voivodeship) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(postalCode, address.postalCode) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(houseNumber, address.houseNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, voivodeship, city, postalCode, street, houseNumber);
+    }
 }
