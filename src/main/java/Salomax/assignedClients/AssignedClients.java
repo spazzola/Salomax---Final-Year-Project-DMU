72b991b54b98a9f1238514b463d15e1cf5c8191f
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-10 15:20:39 +0000

    Added AssignedClients table

---
+++ src/main/java/Salomax/assignedClients/AssignedClients.java
@@ -0,0 +1,27 @@
+package Salomax.assignedClients;
+
+import Salomax.client.Client;
+import Salomax.studio.Studio;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "assignedClients")
+@Data
+public class AssignedClients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assigned_client_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "studio_fk")
+    private Studio studio;
+
+    @ManyToOne
+    @JoinColumn(name = "client_fk")
+    private Client client;
+
+}
