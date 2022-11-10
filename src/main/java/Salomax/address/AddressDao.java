6fafa8edc0f36e255dac031d2038bbe122e0a802
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-10 14:08:28 +0000

    Added AddressDao

---
+++ src/main/java/Salomax/address/AddressDao.java
@@ -0,0 +1,9 @@
+package Salomax.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressDao extends JpaRepository <Address, Long> {
+
+}
