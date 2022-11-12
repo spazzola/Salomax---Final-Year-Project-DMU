fd1d2044bd3fca4aa746f95e5315c7403e120203
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-12 15:28:15 +0000

    Added updateEmployee method. Refactor

--- src/main/java/Salomax/employee/EmployeeService.java
+++ src/main/java/Salomax/employee/EmployeeService.java
@@ -19,17 +19,57 @@ public class EmployeeService {
             throw new IllegalArgumentException(messageException);
         }
         Employee employee = buildEmployeeObject(employeeDto);
-        employee.setWorkRole(WorkRole.EMPLOYEE);
+        grantPrivileges(employee, employeeDto.getWorkRole().getRole());
+        employee.setAssignedStudio(employeeDto.getAssignedStudio());
 
         return employeeDao.save(employee);
     }
 
-    public Employee createAdmin(EmployeeDto employeeDto) {
-        validateEmployee(employeeDto);
+    public Employee createOwner(EmployeeDto employeeDto) {
+        String messageException = validateEmployee(employeeDto);
+        if (messageException.length() > 1) {
+            throw new IllegalArgumentException(messageException);
+        }
         Employee admin = buildEmployeeObject(employeeDto);
-        admin.setWorkRole(WorkRole.ADMIN);
+        admin.setWorkRole(WorkRole.OWNER);
 
         return employeeDao.save(admin);
+    }
+
+    public Employee updateEmployee(EmployeeDto employeeDto) {
+        String messageException = validateEmployee(employeeDto);
+        if (messageException.length() > 1) {
+            throw new IllegalArgumentException(messageException);
+        }
+        Employee employee = employeeDao.findById(employeeDto.getId())
+                .orElseThrow();
+
+        if (!employee.getLogin().equals(employeeDto.getLogin())) {
+            employee.setLogin(employeeDto.getLogin());
+        }
+        if (!employee.getPassword().equals(employeeDto.getPassword())) {
+            //TODO implement passwords' comparing
+        }
+        if (!employee.getName().equals(employeeDto.getName())) {
+            employee.setName(employeeDto.getName());
+        }
+        if (!employee.getSurname().equals(employeeDto.getSurname())) {
+            employee.setSurname(employeeDto.getSurname());
+        }
+        if (!employee.getPhoneNumber().equals(employeeDto.getPhoneNumber())) {
+            employee.setPhoneNumber(employeeDto.getPhoneNumber());
+        }
+        if (!employee.getEmail().equals(employeeDto.getEmail())) {
+            employee.setEmail(employeeDto.getEmail());
+        }
+        if (!employee.getWorkRole().equals(employeeDto.getWorkRole())) {
+            employee.setWorkRole(employeeDto.getWorkRole());
+        }
+        if (!employee.getNote().equals(employeeDto.getNote())) {
+            employee.setNote(employeeDto.getNote());
+        }
+
+        return employee;
     }
 
     public String validateEmployee(EmployeeDto employeeDto) {
@@ -65,6 +105,20 @@ public class EmployeeService {
                 .phoneNumber(employeeDto.getPhoneNumber())
                 .email(employeeDto.getEmail())
                 .build();
+    }
+
+    private void grantPrivileges(Employee employee, String workRole) {
+        switch (workRole.toUpperCase()) {
+            case "OWNER":
+                employee.setWorkRole(WorkRole.OWNER);
+                break;
+            case "ADMIN":
+                employee.setWorkRole(WorkRole.ADMIN);
+                break;
+            case "EMPLOYEE":
+                employee.setWorkRole(WorkRole.EMPLOYEE);
+                break;
+        }
     }
 
 }
