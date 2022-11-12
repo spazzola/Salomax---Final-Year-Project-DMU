88630c0919875a998b54e4c744e3c041588802f6
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-12 15:03:21 +0000

    Added end-point for update Studio

--- src/main/java/Salomax/studio/StudioController.java
+++ src/main/java/Salomax/studio/StudioController.java
@@ -10,10 +10,17 @@ import org.springframework.web.bind.annotation.*;
 public class StudioController {
 
     private StudioService studioService;
+    private StudioMapper studioMapper;
 
     @PostMapping("/create")
     public CreateStudioResponse createStudio(@RequestBody CreateStudioRequest createStudioRequest) {
         return studioService.createStudioAndAdmin(createStudioRequest);
+    }
+
+    @PutMapping("/update")
+    public StudioDto updateStudio(@RequestBody StudioDto studioDto) {
+        Studio studio = studioService.updateStudio(studioDto);
+        return studioMapper.toDto(studio);
     }
 
 }
