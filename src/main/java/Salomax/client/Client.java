14ba16f0148e5a8959cc20d20bdba42cc830df84
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-20 10:29:26 +0000

    Added Builder constructor and equals() / hashCode() methods to Client

--- src/main/java/Salomax/client/Client.java
+++ src/main/java/Salomax/client/Client.java
@@ -1,13 +1,17 @@
 package Salomax.client;
 
 import Salomax.assignedClients.AssignedClients;
+import Salomax.employee.WorkRole;
+import Salomax.studio.Studio;
 import Salomax.user.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "clients")
@@ -19,9 +23,20 @@ public class Client extends User {
     @OneToMany(mappedBy = "client")
     private List<AssignedClients> assignedClients;
 
+    @Builder
     public Client(Long id, String login, String password, String name, String surname,
                   String phoneNumber, String email) {
         super(id, login, password, name, surname, phoneNumber, email);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode());
     }
 
 }
