f045e89f9989388dfe10a26d91d17fb1361b47a7
Rafal Szczotka <rafalszczotka@wp.pl>
2022-11-23 09:52:55 +0000

    Refactor, added missed Spring's annotation

--- src/main/java/Salomax/work/WorkMapper.java
+++ src/main/java/Salomax/work/WorkMapper.java
@@ -1,8 +1,11 @@
 package Salomax.work;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class WorkMapper {
 
 
