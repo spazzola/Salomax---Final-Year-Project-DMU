d90f45bc82e876297d53ff0d283e77e3ab2125c3
Rafal Szczotka <rafalszczotka@wp.pl>
2022-12-01 17:29:02 +0000

    Added end-points for deleting Work

--- src/main/java/Salomax/work/WorkController.java
+++ src/main/java/Salomax/work/WorkController.java
@@ -1,6 +1,7 @@
 package Salomax.work;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -22,6 +23,12 @@ public class WorkController {
     public WorkDto updateWork(@RequestBody WorkDto workDto) {
         Work work = workService.updateWork(workDto);
         return workMapper.toDto(work);
+    }
+
+    @DeleteMapping("/delete")
+    public HttpStatus deleteWork(@RequestParam("id") Long id) {
+        workService.deleteWork(id);
+        return HttpStatus.OK;
     }
 
 }
