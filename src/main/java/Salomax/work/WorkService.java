1c8ecc50ff02436355c9467c8c6fa8fcc9b8f20c
Rafal Szczotka <rafalszczotka@wp.pl>
2022-12-01 17:20:33 +0000

    Added delete method

--- src/main/java/Salomax/work/WorkService.java
+++ src/main/java/Salomax/work/WorkService.java
@@ -65,6 +65,9 @@ public class WorkService {
         return workDao.save(work);
     }
 
+    public void deleteWork(Long id) {
+        workDao.deleteById(id);
+    }
     private void validate(WorkDto workDto) {
         String messageException = "";
         if (workDto.getName() == null || !validationService.validateString(workDto.getName())) {
